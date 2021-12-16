9f828734106bceeaf90b3902da4b501c303130f3
CMCandidate <CMCandidate@tw.com>
2021-12-16 11:36:19 +0100

    WIP

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const app = express();
 require("./config")(app);
 
 const allRoutes = require("./routes");
+const path = require('path')
 app.use(express.static(path.join(__dirname, "public")))
 app.use("/api", allRoutes);
 app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
